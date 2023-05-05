89526427feb7cc2a4e2bbd82848c687f8dd1a45f
demandsupply <giova.zol92@gmail.com>
2023-05-05 20:35:01 +0200

    created a javavscript function for the search sub menu, fixed the hovering submenus

---
+++ index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("dropdown-content-three");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+}
+
+
